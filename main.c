678079bbf14fd9550c136a690412cbf82354a286
knerini <nerini.krystel@outlook.com>
2024-10-28 16:31:10 +0100

    new feature: ft_strpcy + tests in C OK

--- main.c
+++ main.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 extern size_t ft_strlen(const char *str);
+extern char *ft_strcpy(char *dest, const char *src);
 
-int 	main(int ac, char *av)
+int 	main(int ac, char **av)
 {
-	char *str1 = "Hello word";
+	char *str1 = "Hello world";
 	char *str2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut ex nec neque placerat sagittis at at nulla. Etiam in nulla justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque a mi magna. Vestibulum pellentesque nisi id felis faucibus, a consectetur orci molestie. Etiam tempor sed justo nec hendrerit. Integer quis est leo. Integer rutrum, mauris sit amet tempus dignissim, ligula leo dapibus tortor, a bibendum velit nisl a erat. Integer nibh velit, gravida vitae ut.";
 	char *str3 = "";
+	char *destc1 = NULL, *destc2 = NULL, *destc3 = NULL;
+	char *destasm1 = NULL, *destasm2 = NULL, *destasm3 = NULL;
 
 	if (ac > 1)
-		printf("%s\n", "No arguments are required!");
+		printf("No arguments are required!\n");
 
-	printf("%s\nSECTION FT_STRLEN\n\n", "---------------------------------");
-	printf("%s %s\nlength string 1 = %ld\n", "string 1 = ", str1, ft_strlen(str1));
-	printf("%s %s\nlength string 2 = %ld\n", "string 2 = ", str2, ft_strlen(str2));
-	printf("%s %s\nlength string 3 = %ld\n", "string 3 = ", str3, ft_strlen(str3));
+	printf("--------------------------------\nSECTION FT_STRLEN\n\n");
+	printf("string 1 = %s\nlength string 1 (ft_strlen in asm) = %ld -- length string 1 (strlen in c) = %ld\n", str1, ft_strlen(str1), strlen(str1));
+	printf("string 2 = %s\nlength string 2 (ft_strlen in asm) = %ld -- length string 2 (strlen in c) = %ld\n", str2, ft_strlen(str2), strlen(str2));
+	printf("string 3 = %s\nlength string 3 (ft_strlen in asm) = %ld -- length string 3 (strlen in c) = %ld\n", str3, ft_strlen(str3), strlen(str3));
+
+	printf("\n--------------------------------\nSECTION FT_STRCPY\n\n");
+	destasm1 = malloc(sizeof(char) * (ft_strlen(str1) + 1));
+	destc1 = malloc(sizeof(char) * (ft_strlen(str1) + 1));
+	if (destc1 && destasm1)
+	{
+		printf("ft_strcpy of str1, destasm = %s\n---\nstrcpy of str1, destc = %s\n\n", ft_strcpy(destasm1, str1), strcpy(destc1, str1));
+		free(destc1);
+		free(destasm1);
+		destc1 = destasm1 = NULL;
+	}
+
+	destasm2 = malloc(sizeof(char) * (ft_strlen(str2) + 1));
+	destc2 = malloc(sizeof(char) * (ft_strlen(str2) + 1));
+	if (destc2 && destasm2)
+	{
+		printf("ft_strcpy of str2, destasm = %s\n---\nstrcpy of str2, destc = %s\n\n", ft_strcpy(destasm2, str2), strcpy(destc2, str2));
+		free(destc2);
+		free(destasm2);
+		destc2 = destasm2 = NULL;
+	}
+
+	destasm3 = malloc(sizeof(char) * (ft_strlen(str3) + 1));
+	destc3 = malloc(sizeof(char) * (ft_strlen(str3) + 1));
+	if (destc3 && destasm3)
+	{
+		printf("ft_strcpy of str3, destasm = %s\n---\nstrcpy of str3, destc = %s\n\n", ft_strcpy(destasm3, str3), strcpy(destc3, str3));
+		free(destc3);
+		free(destasm3);
+		destc3 = destasm3 = NULL;
+	}
 
 	return(0);
 }
---
+++ srcs/ft_strcpy.s
@@ -0,0 +1,36 @@
+; Data declaration
+
+section .data
+
+; **********
+
+; Code section
+; Function to return the length of a string
+; Call:
+;	ft_strcpy(dest, src);
+
+; Arguments passed:
+; 1) rdi - address of destination string
+; 2) rsi - address of source string
+
+; Returns:
+; 1) rax - address of destination string
+
+section .text
+global ft_strcpy
+ft_strcpy:
+	xor 	r8, 	r8					; r8 = 0, count initialized to 0
+
+_cpy_loop:
+	mov		al,				[rsi + r8]	; Copy src char in intermediary register
+	mov		[rdi + r8],		al			; Copy the char in intermediary register in the dest
+	inc		r8							; Increment to go to next char
+	test	al,				al			; Check if it's \0
+	jnz 	_cpy_loop					; Process again if it's not \0
+
+_end_of_string:
+	mov		rax, rdi					; Copy the dest in the rax register used for return
+	ret 								; Return dest string in rax register
+
+; Stack protection
+section .note.GNU-stack
