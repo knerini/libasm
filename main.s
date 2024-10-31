8371d99f97a981bd7c0c18ec7cc0be3a0710309b
knerini <nerini.krystel@outlook.com>
2024-10-31 22:49:19 +0100

    Update: main.s with ft_strcpy tests

--- main.s
+++ main.s
@@ -11,7 +11,7 @@ EXIT_SUCCESS	equ		0		; success code
 
 STDOUT 			equ 	1	 	; standard output
 SYS_write 		equ 	1		; call code for write
-SYS_exit		equ		60		; terminate programe
+SYS_exit		equ		60		; call code for exit, terminate programe
 
 ; ----Define variables---
 
@@ -22,6 +22,8 @@ str3			db 		"", LF, NULL
 ; ----Call of extern functions----
 
 extern ft_strlen
+extern ft_strcpy
+
 
 
 ; Code section
@@ -31,7 +33,13 @@ section .text
 global main
 main:
 
+	; -----------------------
 	; ----ft_strlen tests----
+	; -----------------------
+
+	; ++++++++++++++
+	; ++++ str1 ++++
+	; ++++++++++++++
 
 	lea		rdi,	[rel str1]	; address of str1
 	call 	ft_strlen
@@ -45,6 +53,10 @@ main:
 	mov		rdx,	rcx			; system write needs the length to count in rdx register
 	syscall
 
+	; ++++++++++++++
+	; ++++ str2 ++++
+	; ++++++++++++++
+
 	lea		rdi,	[rel str2]	; address of str2
 	call 	ft_strlen
 	mov		rbx,	rdi			; save address in another register to get back rdi
@@ -56,6 +68,10 @@ main:
 	mov 	rsi,	rbx			; system write needs the address of string in rsi register
 	mov		rdx,	rcx			; system write needs the length to count in rdx register
 	syscall
+
+	; ++++++++++++++
+	; ++++ str3 ++++
+	; ++++++++++++++
 
 	lea		rdi,	[rel str3]	; address of str3
 	call 	ft_strlen
@@ -69,11 +85,128 @@ main:
 	mov		rdx,	rcx			; system write need sthe length to count in rdx register
 	syscall
 
+
+
+	; -----------------------
+	; ----ft_strcpy tests----
+	; -----------------------
+
+	; ++++++++++++++
+	; ++++ str1 ++++
+	; ++++++++++++++
+
+	lea		rdi,	[rel str1]	; address of str1
+	call 	ft_strlen
+	mov		rcx,	rax			; save ft_strlen result in another register to get back rax
+	add 	rcx,	1 			; add 1 to the length for \0
+
+	; prepare the memory allocation on the stack to obtain a multiple of 16 to keep alignment
+	add 	rcx,	15			; add 15 for alignment
+	and 	rcx,	-16			; AND operation to get the multiple of 16 that is greater than or equal
+	mov		r8,		rcx			; store aligned length to free the memory later
+
+	; memory allocation on the stack
+	sub		rsp,	rcx			; reserving the length in rcx register 
+	mov 	rdi,	rsp			; load destination address
+	lea		rsi,	[rel str1]	; address of str1
+	
+	; ft_strcpy call
+	call 	ft_strcpy
+
+	; write the dest using ft_strlen
+	mov 	rdi, 	rax			; save the result of ft_strcpy in rdi to use ft_strlen
+	call 	ft_strlen
+	mov 	rbx,	rdi			; save address in another register to get back rdi register
+	mov 	rcx,	rax			; save ft_strlen result to get back the rax register
+	mov 	rax, 	SYS_write
+	mov 	rdi,	STDOUT
+	mov 	rsi, 	rbx
+	mov 	rdx,	rcx
+	syscall
+
+	; free the memory allocation of the stack
+	add 	rsp,	r8			; get back rsp register with the same amount of memory
+
+	; ++++++++++++++
+	; ++++ str2 ++++
+	; ++++++++++++++
+
+	lea		rdi,	[rel str2]	; address of str2
+	call 	ft_strlen
+	mov		rcx,	rax			; save ft_strlen result in another register to get back rax
+	add 	rcx,	1 			; add 1 to the length for \0
+
+	; prepare the memory allocation on the stack to obtain a multiple of 16 to keep alignment
+	add 	rcx,	15			; add 15 for alignment
+	and 	rcx,	-16			; AND operation to get the multiple of 16 that is greater than or equal
+	mov		r8,		rcx			; store aligned length to free the memory later
+
+	; memory allocation on the stack
+	sub		rsp,	rcx			; reserving the length in rcx register 
+	mov 	rdi,	rsp			; load destination address
+	lea		rsi,	[rel str2]	; address of str2
+	
+	; ft_strcpy call
+	call 	ft_strcpy
+
+	; write the dest using ft_strlen
+	mov 	rdi, 	rax			; save the result of ft_strcpy in rdi to use ft_strlen
+	call 	ft_strlen
+	mov 	rbx,	rdi			; save address in another register to get back rdi register
+	mov 	rcx,	rax			; save ft_strlen result to get back the rax register
+	mov 	rax, 	SYS_write
+	mov 	rdi,	STDOUT
+	mov 	rsi, 	rbx
+	mov 	rdx,	rcx
+	syscall
+
+	; free the memory allocation of the stack
+	add 	rsp,	r8			; get back rsp register with the same amount of memory
+
+	; ++++++++++++++
+	; ++++ str3 ++++
+	; ++++++++++++++
+
+	lea		rdi,	[rel str3]	; address of str3
+	call 	ft_strlen
+	mov		rcx,	rax			; save ft_strlen result in another register to get back rax
+	add 	rcx,	1 			; add 1 to the length for \0
+
+	; prepare the memory allocation on the stack to obtain a multiple of 16 to keep alignment
+	add 	rcx,	15			; add 15 for alignment
+	and 	rcx,	-16			; AND operation to get the multiple of 16 that is greater than or equal
+	mov		r8,		rcx			; store aligned length to free the memory later
+
+	; memory allocation on the stack
+	sub		rsp,	rcx			; reserving the length in rcx register 
+	mov 	rdi,	rsp			; load destination address
+	lea		rsi,	[rel str3]	; address of str3
+	
+	; ft_strcpy call
+	call 	ft_strcpy
+
+	; write the dest using ft_strlen
+	mov 	rdi, 	rax			; save the result of ft_strcpy in rdi to use ft_strlen
+	call 	ft_strlen
+	mov 	rbx,	rdi			; save address in another register to get back rdi register
+	mov 	rcx,	rax			; save ft_strlen result to get back the rax register
+	mov 	rax, 	SYS_write
+	mov 	rdi,	STDOUT
+	mov 	rsi, 	rbx
+	mov 	rdx,	rcx
+	syscall
+
+	; free the memory allocation of the stack
+	add 	rsp,	r8			; get back rsp register with the same amount of memory
+
+
+
+	; --------------------
 	; ----program done----
+	; --------------------
 	mov 	rax,	SYS_exit
 	mov 	rdi,	EXIT_SUCCESS
 	syscall
 
-
-
-
+; Stack protection
+section .note.GNU-stack
