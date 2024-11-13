0b0e60897325c0b1c1058c7f151a2136e8912512
knerini <nerini.krystel@outlook.com>
2024-11-13 18:50:09 +0100

    Update : add more precise comment

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -20,17 +20,17 @@ extern __errno_location		; call the errno_location function to set the error for
 section .text
 global ft_write
 ft_write:
-	mov		rax, 	1			; Call code for write system call
+	mov		rax, 	1					; Call code for write system call
 	syscall
-	cmp 	rax, 	0			; Compare the rax register value to 0, after syscall rax contains the number of bytes written or an error code
-	jg		_end				; Jump to the end section if rax value is greater than 0
+	cmp 	rax, 	0					; Compare the rax register value to 0, after syscall rax contains the number of bytes written or an error code
+	jg		_end						; Jump to the end section if rax value is greater than 0
 
 _error:
-	neg		rax					; As it's an error code (negative), get the absolute value to get the error code
-	mov		r8,		rax			; Save the result of the write syscall in another register
-	call 	__errno_location WRT ..plt	; Call the extern C function __errno_location to get the pointer to the errno variable
-	mov		[rax],	r8			; As the address of errno variable is in rax register, put the previously saved error code in it by dereferencing the address
-	mov		rax,	-1			; As it's an error, the return has to be '-1'
+	neg		rax							; As it's an error code (negative), get the absolute value to get the error code
+	mov		r8,		rax					; Save the result of the write syscall in another register
+	call 	__errno_location WRT ..plt	; Call the extern C function __errno_location to get the pointer to the errno variable (WRT = With Respect To // plt = function call via Procedure Linkage Table)
+	mov		[rax],	r8					; As the address of errno variable is in rax register, put the previously saved error code in it by dereferencing the address
+	mov		rax,	-1					; As it's an error, the return has to be '-1'
 
 _end:
 	ret
